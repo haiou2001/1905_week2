05627145daaf76225a8deb296d38f945dbbb239c
haiou <haiou@localhost.localdomain>
2020-01-06 10:49:50 +0800

    123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    API-1905
+                    1905_WEEK
                 </div>
 
                 <div class="links">
